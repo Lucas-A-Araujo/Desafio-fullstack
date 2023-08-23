debd8e5181e2e0a67cbd6d65d3e4f5b738d02896
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-08-23 16:01:10 -0300

    chore(users): create users service

---
+++ server/src/modules/users/entities/users.entity/users.entity.ts
@@ -0,0 +1,8 @@
+export class User {
+  id: number;
+  name: string;
+  email: string;
+  cpf: string;
+  celular?: string;
+  knowledge: string[];
+}
--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Delete, Injectable, Param } from '@nestjs/common';
+import { User } from './entities/users.entity/users.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Lucas',
+      email: 'mail@mail.com',
+      cpf: '000.000.000-00',
+      celular: '(82) 90000-0000',
+      knowledge: [
+        'Git',
+        'React',
+        'PHP',
+        'NodeJS',
+        'DevOps',
+        'Banco de Dados',
+        'TypeScript',
+      ],
+    },
+  ];
+
+  getAll() {
+    return this.users;
+  }
+
+  getOne(id: string) {
+    return this.users.find((user) => user.id === Number(id));
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    const indexCourse = this.users.findIndex((user) => user.id === Number(id));
+    return indexCourse;
+  }
+}
