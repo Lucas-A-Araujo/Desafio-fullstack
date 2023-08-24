bb87ee387debfb07dc00848d9a627430e2f4b9dd
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-08-24 19:01:31 -0300

    chore(form): validate page style

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ValidateComponent } from './components/validate/validate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    ValidateComponent
   ],
   imports: [
     BrowserModule,
