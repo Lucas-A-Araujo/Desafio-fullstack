fac05cfa6c3a4cdc31d5683dfc1191f21ccf4e3d
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-08-24 20:18:03 -0300

    feat(form): list users on the records page

--- client/src/app/components/records/records.component.ts
+++ client/src/app/components/records/records.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecordsService } from 'src/app/services/records.service';
 
 @Component({
   selector: 'app-records',
   templateUrl: './records.component.html',
   styleUrls: ['./records.component.css']
 })
-export class RecordsComponent {
+export class RecordsComponent implements OnInit {
+  users: any[] = [];
 
+  constructor(private recordsService: RecordsService) { }
+
+  ngOnInit() {
+    this.recordsService.getUsers().subscribe((data: any) => {
+      console.log('aaaaaa')
+      this.users = data;
+      console.log(data)
+    });
+  }
 }
