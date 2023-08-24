f036ae835b90c2c1123a1e5c815d89b31be63336
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-08-24 19:03:03 -0300

    feat(form): create routes to records page and validate page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
-
+import { ValidateComponent } from './components/validate/validate.component';
+import { RecordsComponent } from './components/records/records.component';
+//RecordsComponent
 const routes: Routes = [{
   path: ':nomeColaborador/registrar',
   component: RegisterComponent
-}];
+},
+{
+  path: ':nomeColaborador/validar',
+  component: ValidateComponent
+},
+{
+  path: 'registros',
+  component: RecordsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
