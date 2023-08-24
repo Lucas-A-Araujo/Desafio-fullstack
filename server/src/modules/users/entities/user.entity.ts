81453ffec1ad6d3f8374f0452fafb5c1bd915f5f
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-08-24 06:08:48 -0300

    chore(users): config users tables

--- server/src/modules/users/entities/user.entity.ts
+++ server/src/modules/users/entities/user.entity.ts
@@ -1,8 +1,32 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Knowledge } from './knowledge.entity';
+
+@Entity('courses' /*Nome da tabela */)
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ length: 100 })
   name: string;
+
+  @Column({ length: 100 })
   email: string;
+
+  @Column({ length: 14 })
   cpf: string;
+
+  @Column({ length: 15, nullable: true })
   celular?: string;
+
+  @JoinTable()
+  @ManyToMany(() => Knowledge, (knowledge) => knowledge.users, {
+    cascade: true,
+  })
   knowledge: string[];
 }
