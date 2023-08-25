bfcb07085b611e98b0e2410f1ec79c2890425bf3
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-08-25 14:49:22 -0300

    test(form): make ValidateComponent tests

--- client/src/app/components/validate/validate.component.spec.ts
+++ client/src/app/components/validate/validate.component.spec.ts
@@ -1,21 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ValidateComponent } from './validate.component';
+import { of } from 'rxjs';
+import { ValidateService } from 'src/app/services/validate.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ValidateComponent', () => {
   let component: ValidateComponent;
   let fixture: ComponentFixture<ValidateComponent>;
+  let validateServiceSpy: jasmine.SpyObj<ValidateService>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('ValidateService', ['getUserByName', 'changeUserStatus']);
+
     TestBed.configureTestingModule({
-      declarations: [ValidateComponent]
+      declarations: [ValidateComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: { params: of({ nomeColaborador: 'testUser' }) } },
+        { provide: ValidateService, useValue: spy }
+      ]
     });
+
     fixture = TestBed.createComponent(ValidateComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    validateServiceSpy = TestBed.inject(ValidateService) as jasmine.SpyObj<ValidateService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch user data and format it', () => {
+    const userData = {
+      id: 1,
+      name: 'Test User',
+      email: 'test@example.com',
+      cpf: '12345678900',
+      celular: '0012345678',
+      status: true,
+      knowledge: [{ name: 'Angular' }, { name: 'React' }]
+    };
+
+    validateServiceSpy.getUserByName.and.returnValue(of(userData));
+
+    component.ngOnInit();
+
+    expect(validateServiceSpy.getUserByName).toHaveBeenCalled();
+    expect(component.user).toEqual({
+      ...userData,
+      cpf: '123.456.789-00',
+      celular: '(00) 1234-5678'
+    });
   });
 });
